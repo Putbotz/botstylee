2773cbd927b8ff2913d3d5ecb172bbcec0a37453
Putbotz <96361236+Putbotz@users.noreply.github.com>
2022-01-18 23:01:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/KihEAYjKr04LI4uUrbiiip']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62895368900456': { // put your number here
-    name: 'benniismael',
+  '60147366955': { // put your number here
+    name: 'Putbotz',
     isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
@@ -59,8 +59,8 @@ fs.watchFile(file, () => {
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'Benniganteng \'ig : Beni_230'
-  var sticker_author = 'Benniismael'
+  var sticker_name = 'Putbotz'
+  var sticker_author = 'Ganteng'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -77,7 +77,7 @@ fs.watchFile(file_exif, () => {
 global.packname = sticker_name
 global.author = sticker_author
 global.wait = '_*tunggu..*_'
-global.eror = '_*Server Error*_'
+global.eror = '_*Error*_'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
 
